a0f15d92ffe9910effdc3cfb6727b38d38e9d226
Lawrence <lpinto@globalmed.com>
2023-07-10 12:32:11 +0530

    more code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
+// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
+// To get the root element from the HTML document
+import sublime from './images/sublime.png'
 
-const jsxElement = <h1>This is a JSX element</h1>
+const user = (
+  <div>
+    <img src={sublime} alt='sublime' />
+  </div>
+)
+
+// JSX element, main
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      {user}
+    </div>
+  </main>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {main}
+  </div>
+)
+
 const rootElement = document.getElementById('root')
-
-ReactDOM.render(jsxElement, rootElement)
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
